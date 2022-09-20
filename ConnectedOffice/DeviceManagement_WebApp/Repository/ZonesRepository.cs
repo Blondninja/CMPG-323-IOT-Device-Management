5f4ae34c2048f8f748c694202d285a3be2637d17
Blondninja <rehanpervis@gmail.com>
2022-09-20 21:32:41 +0200

    Added Repository class(Teir 1) and the interfaces(Teir 2)

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZonesRepository.cs
@@ -0,0 +1,34 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZonesRepository
+    {
+        private readonly ConnectedOfficeContext _context;
+
+        // GET: Categories
+        public List<Zone> GetAll()
+        {
+            return _context.Zone.ToList();
+        }
+
+        // GET: Categories/Details/5
+        public async Task<Zone> GetById(Guid? id)
+        {
+            
+
+            var zone = await _context.Zone
+                .FirstOrDefaultAsync(m => m.ZoneId == id);
+            
+
+            return (zone);
+        }
+    }
+}
