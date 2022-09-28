c6a7a8a8d4926b18118ef88b285ff747920c926b
Blondninja <rehanpervis@gmail.com>
2022-09-28 15:29:55 +0200

    added gitignore and make changes on the codes

--- ConnectedOffice/DeviceManagement_WebApp/Models/Category.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -14,11 +15,14 @@ namespace DeviceManagement_WebApp.Models
         {
             Device = new HashSet<Device>();
         }
+        [Required]
 
         [DisplayName("Category ID")]
         public Guid CategoryId { get; set; }
+        [Required]
         [DisplayName("Category Name")]
         public string CategoryName { get; set; }
+        [Required]
         [DisplayName("Category Description")]
         public string CategoryDescription { get; set; }
         [DisplayName("Category Date Created")]
--- ConnectedOffice/DeviceManagement_WebApp/Models/Device.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Models/Device.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -12,6 +13,8 @@ namespace DeviceManagement_WebApp.Models
     {
         [DisplayName("Device ID")]
         public Guid DeviceId { get; set; }
+
+        [Required]
         [DisplayName("Device Name")]
         public string DeviceName { get; set; }
         [DisplayName("Category ID")]
